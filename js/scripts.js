23b148cc0220ac637640c4b3829706d10e70be13
katj2950 <katj2950@stud.kea.dk>
2020-03-18 13:11:25 +0100

    bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(function () { // DOM ready for jQuery??
+  //write your code below
+
+  //The two go to debugging tools: alert & console.log - uncomment them using // or /* .... */
+  //alert("hey");
+  console.log('hi there from "js/scripts.js" - good to see you');
+  console.error('Just checking if YOU are checking the console. You should always open Chrome DevTools and check the console when doing web development. \n\nPlease uncomment or delete these messages from the js-file. \n\nBUT remember the syntax for alert and console.log \(e.g. by keeping then in the comments in the file\). \n\n//Lasse');
+}); //END DOM ready
